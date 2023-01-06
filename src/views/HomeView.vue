3135a14ede7b5ae78a452f0723af6fcfd76ded22
TesterCC <testerlyx@foxmail.com>
2023-01-06 10:11:10 +0800

    update - learn vue api code and notes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,94 @@
 <template>
-  <div>
+  <!-- <div>
     <nav-header></nav-header>
     <nav-main></nav-main>
     <nav-footer></nav-footer>
+  </div> -->
+  <div>
+    {{ num }}
+  </div>
+  <div>
+    {{ name }}
+  </div>
+  <div>
+    {{ arr }}
+  </div>
+  <div>
+    {{ arr[0] }}
+  </div>
+  <div>
+    {{ arr.slice(0, 2) }}
+  </div>
+  <div>
+    {{ obj }}
+  </div>
+  <div>
+    {{ obj.age }}
+  </div>
+  <hr>
+  <div>
+    {{ data }}
+  </div>
+  <div>
+    {{ data.ip }}
+  </div>
+  <div>
+    {{ data.obj }}
+  </div>
+  <div>
+    {{ data.obj.update_time }}
   </div>
 
 </template>
 
 <script>
 // 将子组件引入到父组件中
-import NavHeader from '@/components/navHeader/NavHeader'
-import NavMain from '@/components/navMain/NavMain'
-import NavFooter from '@/components/navFooter/NavFooter'
+// import NavHeader from '@/components/navHeader/NavHeader'
+// import NavMain from '@/components/navMain/NavMain'
+// import NavFooter from '@/components/navFooter/NavFooter'
 
 // 编写js内容
-import { defineComponent } from 'vue'
+import { defineComponent, ref, reactive } from 'vue'
 
 export default defineComponent({
   name: 'Home', // 组件名称
-  // // 接收父组件的数据
-  props:{
+  // 接收父组件的数据
+  props: {
 
   },
   // 定义子组件
   components: {
-    NavHeader,
-    NavMain,
-    NavFooter
+    // NavHeader,
+    // NavMain,
+    // NavFooter
   },
-  setup(props,ctx){
+  setup(props, ctx) {
+    let num = ref(10)
+    let name = ref('Tester')
+    let arr = ref(["python", "go", "java", "php", "js"])   // 定义数组
+    // 定义对象
+    let obj = ref({
+      age: 20
+    })
 
+    // 一次性定义多个数据用reactive
+    let data = reactive({
+      ip:"127.0.0.1",
+      port:5000,
+      obj: {
+        create_time: 1672960084,
+        update_time: 1672970788,
+      }
+    })
+
+    // 将定义好的数据 return 出去, 否则页面上不会展示
+    return {
+      num,
+      name,
+      arr,
+      obj,
+      data
+    }
   }
 })
 
