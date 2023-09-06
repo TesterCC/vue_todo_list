442117d3ff2a9ee782128c105c5e807638131471
TesterCC <testerlyx@foxmail.com>
2023-09-06 14:34:41 +0800

    update - parent and child component data exchange

--- src/components/child/Child.vue
+++ src/components/child/Child.vue
@@ -13,9 +13,11 @@
 
 
 <script>
-import { defineComponent,ref } from 'vue';
+import { defineComponent, onMounted, ref, withCtx } from 'vue';
 
-// 4-5 
+// 父子组件之间如何传递参数：
+// 父组件 -> 子组件：父组件通过动态绑定属性的方式，子组件在props中接收
+// 子组件 -> 父组件：子组件通过ctx.emit分发事件的方式，在父组件template的子组件标签中绑定自定义事件，自定义事件中还有从子组件传递过来的参数
 export default defineComponent({
     name: 'Child',
     // 接收父组件传递过来的参数，注意props接收的数据也不能直接改
@@ -29,13 +31,23 @@ export default defineComponent({
             // default: 'default value'
         }
     },
-    setup(props) {
+    setup(props, ctx) {
         let childMsg = ref('Here is child component data')
+        let childNum = ref(10)
         console.log(props.msg)
         let send = () => {
             // 通过ctx.emit分发事件  4-5 08:30
+            // emit 第一个参数是事件名称 第二个是传递的数据(只能传一个数据，传递多个数据需要使用数组或者对象)
+            // ctx.emit('send', childMsg.value) // 把childMsg传递给父组件
+            // ctx.emit('send', [childMsg.value, childNum.value]) // method1：使用数组Array传递多个数据
+            ctx.emit('send', {msg:childMsg.value, num:childNum.value}) // method1：使用对象{}传递多个数据
 
         }
+
+        // 分发不一定要点击事件，页面加载也可以分发
+        onMounted(() => {
+            ctx.emit('send', childMsg.value)
+        })
         return {
             childMsg,
             send
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,7 +2,8 @@
     <div>
         <h1>This is detail component</h1>
         <!-- 父组件传值给子组件 需要用数据绑定 -->
-        <child :msg='msg'></child>
+        <!-- @send是子组件分发过来的，'send'是父组件自定义的 -->
+        <child :msg='msg' @send='send'></child>
     </div>
 </template>
 
@@ -21,8 +22,12 @@ export default defineComponent({
     },
     setup() {
         let msg = ref("This is parent component data")
+        let send = (val) => {
+            console.log(val)
+        }
         return {
-            msg
+            msg,
+            send
         }
     }
 })
